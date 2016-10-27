7ea7f44ffa27051782d35b13b008c4366d874cd3
pro-gramma <jamesneverson@gmail.com>
2016-10-27 00:24:53 -0400

    Update repositoryBackup.sh

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -1,7 +1,7 @@
 #!bin/bash
+#cron
 git clone https://github.com/pro-gramma/cisc220_2.git cisc220_2
 read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
-FILE=$(tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz" $1)
-git add FILE
+git add $(grep -l #cron | tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz" $1)
 git commit
 git push https://$3:$4@github.com/repoURL.git --all
