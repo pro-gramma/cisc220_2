66a32d758e895b7de7a955f2f8419e3eae170bc9
pro-gramma <jamesneverson@gmail.com>
2016-10-27 02:41:44 -0400

    Update repositoryBackup.sh

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -4,4 +4,3 @@ read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
 git add $(ls -I backup*.tgz | xargs tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz" $1)
 git commit -m "Commit ${DD}/${MM}/${YY}"
 git push https://$3:$4@$2 --all
-chmod +x repositoryBackup.sh
