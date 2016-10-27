634513e668e8993ef3978fbff9243a8ea91e16aa
pro-gramma <jamesneverson@gmail.com>
2016-10-27 00:42:17 -0400

    Update repositoryBackup.sh

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -5,4 +5,3 @@ read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
 git add $(grep -l #cron | tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz" $1)
 git commit
 git push https://$3:$4@github.com/repoURL.git --all
-chmod 744
