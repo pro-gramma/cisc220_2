3cb7ac263edc626275af6de32499cf0d1b1294d1
pro-gramma <15jn7@queensu.ca>
2016-10-27 03:21:59 -0400

    Auto-backup 27/10/2016

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -3,6 +3,6 @@
 read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
 cd $1
 git add $(ls -I backup*.tgz | xargs tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz")
+chmod 777 "backup${YYYY}${MM}${DD}${HH}.tgz"
 git commit -m "Auto-backup ${DD}/${MM}/${YYYY}"
 git push https://$3:$4@$2 --all
-chmod 777 "backup${YYYY}${MM}${DD}${HH}.tgz"
