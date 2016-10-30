0198eccf7d96b73859eb5f73bc3ae6877d778d6e
pro-gramma <15jn7@queensu.ca>
2016-10-30 19:54:37 -0400

    Auto-backup 30/10/2016

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -4,9 +4,9 @@ git add backupsLog
 read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
 cd $1
 FILE=$(ls -I backup*.tgz | xargs tar -cvzf "backup${YYYY}${MM}${DD}${HH}.tgz")
-chmod 777 $FILE
 git add $FILE
 updatedb
+chmod 777 $FILE
 echo "Backup backup${YYYY}${MM}${DD}${HH}.tgz created successfully!" 1>> backupsLog
 git commit -m "Auto-backup ${DD}/${MM}/${YYYY}"
 echo "Backup backup${YYYY}${MM}${DD}${HH}.tgz comitted to the local git repository" 1>> backupsLog
