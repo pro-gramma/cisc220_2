06a7be53dff24c2f2514d82c3a71263d191217c2
pro-gramma <15jn7@queensu.ca>
2016-10-30 20:02:21 -0400

    Auto-backup 30/10/2016

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -7,7 +7,5 @@ git add $FILE
 echo "Backup backup${YYYY}${MM}${DD}${HH}.tgz created successfully!" 1>> backupsLog
 git commit -m "Auto-backup ${DD}/${MM}/${YYYY}"
 echo "Backup backup${YYYY}${MM}${DD}${HH}.tgz comitted to the local git repository" 1>> backupsLog
-updatedb
-chmod 777 $FILE
 git push https://$3:$4@$2 --all
 echo "Backup backup${YYYY}${MM}${DD}${HH}.tgz pushed to the remote git repository $1" 1>> backupsLog
