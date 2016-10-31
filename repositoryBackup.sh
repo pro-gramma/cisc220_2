38ff38191db14c3e13ed4710b637c8b3dfadda1d
pro-gramma <15jn7@queensu.ca>
2016-10-30 21:21:14 -0400

    more repo edits

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 #cron
+cron
 cd $1
 git add backupsLog
 read YYYY MM DD HH <<<$(date +'%Y %m %d %I')
@@ -7,7 +8,7 @@ FILE="backup${YYYY}${MM}${DD}${HH}.tgz"
 ls -I backup*.tgz | xargs tar -cvzf $FILE
 git add $FILE
 echo "Backup ${FILE}.tgz created successfully!" 1>> backupsLog
-git commit -m "Auto ${FILE}"
+git commit -m "Auto ${DD}/${MM}/${YYYY}"
 echo "Backup ${FILE}.tgz comitted to the local git repository" 1>> backupsLog
 updatedb
 echo "Backup ${FILE}.tgz pushed to the remote git repository $1" 1>> backupsLog
