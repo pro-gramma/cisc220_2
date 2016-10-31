605d4b14dd05747832628c0f6b1e7ed3b500fd2f
pro-gramma <jamesneverson@gmail.com>
2016-10-31 00:44:20 -0400

    Update queensuComputingSubdomainFinder.sh

--- queensuComputingSubdomainFinder.sh
+++ queensuComputingSubdomainFinder.sh
@@ -1,1 +1,10 @@
 #!bin/bash
+echo -e "IP Address\tSub-Domain"
+for ((i=0;i<=255;i++));do
+	for ((ii=0;ii<=255;ii++));do
+		output=($(nslookup 130.15.${i}.${ii}))
+		if [[ "${output[9]}" == *"cs.queensu.ca." ]];then
+			echo -e "130.15.${i}.${ii}\t${output[9]}"
+		fi
+	done
+done
